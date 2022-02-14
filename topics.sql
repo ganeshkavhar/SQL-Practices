9c2e53bc4b4dd87b5017514af9d9c1847dc0a298
ganesh kavhar <kavharg@gmail.com>
2022-02-14 06:15:48 +0530

    Add files via upload

---
+++ topics.sql
@@ -0,0 +1,18 @@
+1.	Display the first name and join date of the employees who joined between 2002 and 2005.
+2.	Display first name and join date of the employees who is either IT Programmer or Sales Man.
+3.	Display employees who joined after 1st January 2008.
+4.	Display details of employee with ID 150 or 160.
+5.	Display first name, salary, commission pct, and hire date for employees with salary less than 10000.
+6.	Display details of jobs in the descending order of the title.
+7.	Display employees where the first name or last name starts with S.
+8.	Display employees who joined in the month of May.
+9.	Display details of the employees where commission percentage is null and salary 
+    in the range 5000 to 10000 and department is 30.
+10.	Display first name and last name after converting the first letter of each name 
+	to upper case and the rest to lower case
+11. Diplay first 5 records from employees
+12. Display the details of employee who get 3rd highest salary
+13. Display the details of employee who get lowest salary	
+14. Display the details of last five employees.
+15. Create an empty table same as employees.
+16. Insert the data into new empty table from employees.
